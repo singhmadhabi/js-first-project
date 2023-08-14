7a23eeed0677fbeef057ecd6ed6e7f7c83aa3718
Madhabi Singh <singhmadhabi4@gmail.com>
2023-08-14 23:17:27 +0545

    var and interactions

--- indext.js
+++ indext.js
@@ -1,1 +1,14 @@
-console.log("Welcome");
+//console.log("Welcome");
+var person = "Madhabi";
+
+person= prompt("Enter Your Name","UserName");
+let password = prompt("Enter Your Password"," ");
+const isLegal = confirm("Are you the member of Broadway Infosys?");
+
+if(isLegal && person === "Madhabi") {
+    alert("Welcome to the class");
+}
+if(isLegal) {
+    alert("SORRY! Access Denied");
+}
+console.log(person, password, isLegal);
