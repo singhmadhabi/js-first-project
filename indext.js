454771c02643e6038dd8e78e88bb033e9f2922dd
Madhabi Singh <singhmadhabi4@gmail.com>
2023-08-15 08:45:37 +0545

    if else, switch case and ternary operator

--- indext.js
+++ indext.js
@@ -1,5 +1,5 @@
 //console.log("Welcome");
-var person = "Madhabi";
+const person = "Madhabi";
 
 person= prompt("Enter Your Name","UserName");
 let password = prompt("Enter Your Password"," ");
@@ -8,7 +8,34 @@ const isLegal = confirm("Are you the member of Broadway Infosys?");
 if(isLegal && person === "Madhabi") {
     alert("Welcome to the class");
 }
-if(isLegal) {
+if(!isLegal) {
     alert("SORRY! Access Denied");
 }
 console.log(person, password, isLegal);
+
+//if else
+if (a === 0) {
+} else if (a === 1) {
+} else if (a === 2) {
+} else {
+    alert("Invalid day");
+}
+
+
+//switch case
+switch (a === 0) {
+    case value:
+    case value:
+    default:
+}
+
+//Ternary operator
+
+if (a === 0) {
+    console.log("it's 0");
+} else {
+    console.log("it's not 0");
+}
+
+//Ternary operatory
+const answer = a === 0 ? console.log("it's 0") : console.log("it's not 0");
