5677b5ff5bbae4c0ae089e1a7e4f46dfe3b2f77d
Madhabi Singh <singhmadhabi4@gmail.com>
2023-08-18 09:31:08 +0545

    array,destructure

--- indext.js
+++ indext.js
@@ -173,11 +173,12 @@ do {
   if (i <= 10) {
     console.log(i);
   }
-  i++;
+  i++; //(i = i + 2) or (i += 2)
 } while (i < 10);
 */
 
 //switch case
+/*
 for (let i = 0; i < 10; i++) {
   switch (true) {
     case i <= 10:
@@ -187,3 +188,82 @@ for (let i = 0; i < 10; i++) {
       break;
   }
 }
+*/
+
+/*
+//Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.
+function isMultipleOf3Or7(num) {
+  if (num <= 0) {
+    return false;
+  }
+
+  return num % 3 === 0 || num % 7 === 0;
+}
+
+// Test the function
+const number = 21;
+
+if (isMultipleOf3Or7(number)) {
+  console.log(`${number} is a multiple of 3 or 7.`);
+} else {
+  console.log(`${number} is not a multiple of 3 or 7.`);
+}
+*/
+/*
+//Write a JavaScript program to get the current date.
+function getCurrentDate() {
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+  const day = String(today.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
+
+console.log(getCurrentDate());
+*/
+/*
+const currentDate = new Date();
+console.log(currentDate);
+*/
+/*
+//Write a JavaScript program to count the number of vowels in a given string.
+function countVowels(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countVowels("Madhabi"));
+console.log(countVowels("PranavKushwaha"));
+console.log(countVowels("RaktimShrestha"));
+*/
+
+/*write a function that checks the username and password and send the following messages: 
+if no username, username required, if no password, password required, if username !== username not found,
+if username and passwoerd matches, access granted
+*/
+/*
+//array
+const vehicles = ("car", "bus", "cycle", "truck", "bike");
+console.log(vehicles.length);
+vehicles.push("car");
+console.log(vehicles);
+
+*/
+
+//destructure
+//spread
+const [first, second, third, ...rest] = [1, 2, 3, 4];
+console.log(rest);
+
+const { a, b, ...other } = { a: 1, b: 2, c: 3 };
+console.log(other);
